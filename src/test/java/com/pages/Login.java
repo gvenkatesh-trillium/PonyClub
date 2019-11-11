8d6d6c534467aba96053a7057b9eb3baf26bb1a4
gvenkatesh <gops.gond008@gmai.com>
2019-11-11 11:38:30 +0000

    login test

--- src/test/java/com/pages/Login.java
+++ src/test/java/com/pages/Login.java
@@ -24,7 +24,7 @@ public class Login extends AbstractMain {
     }
 
     public void verifyLoginPage(){
-        Assert.assertTrue(action.getElementText(LoginPage).contains("Login"));
+        Assert.assertTrue(action.getElementText(LoginPage).contains("Login1"));
         Assert.assertTrue(action.getElementText(LoginPage).contains("Username *"));
         Assert.assertTrue(action.getElementText(LoginPage).contains("Password *"));
         Assert.assertTrue(action.getElementText(LoginPage).contains("Forgot your password"));
