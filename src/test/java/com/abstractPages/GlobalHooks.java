0630e6721aaa4e6b586b60dcbe92aab106d96400
gvenkatesh <gops.gond008@gmai.com>
2019-11-12 14:37:29 +0000

    login and Registration test

--- src/test/java/com/abstractPages/GlobalHooks.java
+++ src/test/java/com/abstractPages/GlobalHooks.java
@@ -7,13 +7,25 @@ import cucumber.api.java.Before;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class GlobalHooks extends AbstractMain {
 
    @Before
     public void startTest() {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
+       ChromeOptions options = new ChromeOptions();
+       options.addArguments("disable-infobars");
+       options.addArguments("test-type");
+       options.addArguments("allow-running-insecure-content");
+       DesiredCapabilities caps = DesiredCapabilities.chrome();
+       caps.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+       caps.setCapability(ChromeOptions.CAPABILITY, options);
+       driver = new ChromeDriver(options);
+
+//       driver = new ChromeDriver();
     }
     @After
     public void endTest(Scenario scenario) throws Exception {
